a48a246b3d9bea9d18b8a87bb9aded7d97b15ebb
Sergey Sheviakov <scabo.dev@gmail.com>
2012-07-05 19:08:58 +0400

    implement positive test

--- tests/Pattern/ValueObject/PositiveTest.php
+++ tests/Pattern/ValueObject/PositiveTest.php
@@ -7,4 +7,44 @@ class Pattern_ValueObject_PositiveTest extends PHPUnit_Framework_TestCase
 		$expected = new Pattern_ValueObject_Positive('hello');
 		$this->assertEquals($expected->getValue(), 0);
 	}
+	
+	public function testTryPossibleValue_Integer()
+	{
+		
+		$values = array(
+			'5' => 5,
+			'2.358' => 2,
+			'3568sdfsdf' => 3568,
+			329 => 329,
+			32.656 => 32,
+			false => 0,
+			true => 1,
+			-3239 => 0
+		);
+		
+		foreach ($values as $key => $value) {
+			$positive = new Pattern_ValueObject_Positive($key);
+			$this->assertEquals($positive->getValue(), $value);
+		}
+	}
+	
+	public function testAdd_Integer()
+	{
+		$value = new Pattern_ValueObject_Positive(5);
+		$expect = $value->add(10);
+		$this->assertEquals($expect->getValue(), 15);
+	}
+	
+	public function testByValue()
+	{
+		$value = new Pattern_ValueObject_Positive(5);
+		
+		$obj1 = $value;
+		$obj2 = $value;
+		
+		$obj1 = $obj1->add(20);
+		
+		$this->assertEquals($obj1->getValue(), 25);
+		$this->assertEquals($obj2->getValue(), 5);
+	}
 }
